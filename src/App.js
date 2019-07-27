7496a45419de9682133d663647635b49207c21b3
bernhardwhy <bernhard.wytek@dataformers.at>
2019-07-27 13:15:22 +0200

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,29 +1,76 @@
 import React, { Component } from 'react'
 import './App.css';
-import { DB_CONFIG } from './shared/config';
-import firebase from 'firebase';
+import { firebaseDB } from './shared/config';
 
 export default class App extends Component {
   state = {
-    groceries: 5,
+    groceries: {},
+    name: '',
   }
 
   componentDidMount() {
-    const app = firebase.initializeApp(DB_CONFIG);
-    const database = app.database().ref().child('groceries');
+    
+    const database = firebaseDB.database().ref().child('groceries');
     database.on('value', snap => {
-      this.setState({
-        groceries: snap.val()
-      })
+      console.log(snap.val());
+      this.setState({groceries: snap.val()})
     })
   }
 
+  changeData = (groceryId, isBuyed) => {
+    firebaseDB.database().ref().child('groceries/' + groceryId).update({
+      buyed: !isBuyed
+    });
+  }
+
+  addData = () => {
+    firebaseDB.database().ref().child('groceries/').push({
+      name: this.state.name,
+      buyed: false
+    });
+  }
+
+  enterName = (event) => {
+this.setState({
+  name: event.target.value
+})
+  }
+
+  loopThroughObject = (object) => {
+    for (var key in object) {
+      // skip loop if the property is from prototype
+      if (!object.hasOwnProperty(key)) continue;
+
+      var obj = object[key];
+      for (var prop in obj) {
+        // skip loop if the property is from prototype
+        obj.id = key;
+        if (!obj.hasOwnProperty(prop)) continue;
+        
+      }
+    }
+  }
+
   render() {
+    const renderedGroceries = Object.values(this.state.groceries);
+    this.loopThroughObject(this.state.groceries);
     return (
       <div className="App">
-        <p>
-          {this.state.groceries}
-        </p>
+          {renderedGroceries.map(grocery => {
+            return (
+            <div>
+              <div>
+                <div>
+                  <h1>{grocery.name}</h1>                
+                    <p>{grocery.buyed ? 'bought' : 'to buy'}</p>
+                    <button onClick={() => this.changeData(grocery.id, grocery.buyed)}>update entry</button>
+                </div>
+              </div>
+            </div>
+            )
+          })}
+          <input type="text" onChange={this.enterName} value={this.state.name} placeholder="enter lebensmittel"/>
+        <button disabled={!this.state.name} onClick={this.addData}>add brot</button>
       </div>
     )
   }
--- src/shared/config.js
+++ src/shared/config.js
@@ -1,4 +1,6 @@
-export const DB_CONFIG = {
+import firebase from 'firebase';
+
+const DB_CONFIG = {
     apiKey: "AIzaSyAV9yXqKCH-iu15t41ExuySTvZxtjOW7hw",
     authDomain: "sapp-92c9f.firebaseapp.com",
     databaseURL: "https://sapp-92c9f.firebaseio.com",
@@ -7,3 +9,5 @@ export const DB_CONFIG = {
     messagingSenderId: "176221571558",
     appId: "1:176221571558:web:034c9e35f77d3d5c"
 };
+
+export const firebaseDB = firebase.initializeApp(DB_CONFIG);
