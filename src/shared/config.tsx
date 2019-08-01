b6ee52667fbe8d5adf9f4002a76986698d501678
bernhardwhy <bernhard.wytek@dataformers.at>
2019-08-01 14:05:21 +0200

    receive notification token

--- src/shared/config.tsx
+++ src/shared/config.tsx
@@ -1,13 +1,29 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
 const DB_CONFIG = {
-    apiKey: "AIzaSyAV9yXqKCH-iu15t41ExuySTvZxtjOW7hw",
-    authDomain: "sapp-92c9f.firebaseapp.com",
-    databaseURL: "https://sapp-92c9f.firebaseio.com",
-    projectId: "sapp-92c9f",
-    storageBucket: "",
-    messagingSenderId: "176221571558",
-    appId: "1:176221571558:web:034c9e35f77d3d5c"
+  apiKey: "AIzaSyAV9yXqKCH-iu15t41ExuySTvZxtjOW7hw",
+  authDomain: "sapp-92c9f.firebaseapp.com",
+  databaseURL: "https://sapp-92c9f.firebaseio.com",
+  projectId: "sapp-92c9f",
+  storageBucket: "",
+  messagingSenderId: "176221571558",
+  appId: "1:176221571558:web:034c9e35f77d3d5c"
 };
 
 export const firebaseDB = firebase.initializeApp(DB_CONFIG);
+
+const messaging = firebase.messaging();
+navigator.serviceWorker.ready.then(registeredSW => {
+  messaging.useServiceWorker(registeredSW);
+});
+messaging
+  .requestPermission()
+  .then(() => {
+    console.log("have permission");
+    messaging.getToken().then(token => {
+      console.log("got notification token", token);
+    });
+  })
+  .catch(error => {
+    console.log("ERROR: permission denied", error);
+  });
