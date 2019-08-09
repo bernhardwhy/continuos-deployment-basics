cbcef5690c86a44cb4031827810c81ef1412cd16
bernhardwhy <bernhard.wytek@dataformers.at>
2019-08-09 13:48:40 +0200

    add changes to master

--- src/App.tsx
+++ src/App.tsx
@@ -86,7 +86,7 @@ export default class App extends Component {
     this.loopThroughObject(this.state.groceries);
     return (
       <div className="App">
-        Groceries: v1.2
+        Groceries: v1.3
         <div className="GroceryList">
           {renderedGroceries.map(grocery => {
             return (
